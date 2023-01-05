fcdd2e3987615542d3b195d098511d498e68e7db
unknown <shsps984705@gmail.com>
2023-01-05 22:32:52 +0800

    StepManager V1

--- Assets/Scripts/CatchableItem/Cube.cs
+++ Assets/Scripts/CatchableItem/Cube.cs
@@ -2,19 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cube : MonoBehaviour, ICatchable
+public class Cube : Catchable
 {
-    public bool IsCatching { get; set; } = false;
+    //public bool IsCatching { get; set; } = false;
 
-    public void Catch(GameObject tool)
+    public override void Catch(GameObject tool)
     {
         this.transform.parent = tool.transform;
-        //this.transform.localPosition = Vector3.zero;
         this.GetComponent<Rigidbody>().useGravity = false;
         IsCatching = true;
     }
 
-    public void Release()
+    public override void Release()
     {
         this.transform.parent = null;
         this.GetComponent<Rigidbody>().useGravity = true;
--- Assets/Scripts/StepInfo.cs
+++ Assets/Scripts/StepInfo.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class StepInfo : MonoBehaviour
 {
-    private IKManager3D2.OperationMode mode;
-    private Joint[] joints;
+    public IKManager3D2.OperationMode mode;
+    public IKManager3D2 ik;
+    public Joint[] joints;
+    public float MoveToolAngleX = 0f;
+    public float MoveToolAngleY = 0f;
+    public float MoveToolAngleZ = 0f;
+    public bool IsCatchPressed = false;
 
     public StepInfo() { }
-    public StepInfo(IKManager3D2 iK)
+    public StepInfo(IKManager3D2 ik)
     {
-        mode = iK.mode;
-        joints = iK.joints;
+        mode = ik.mode;
+        this.ik = ik;
+        joints = ik.joints;
+        MoveToolAngleX = ik.MoveToolAngleX;
+        MoveToolAngleY = ik.MoveToolAngleY;
+        MoveToolAngleZ = ik.MoveToolAngleZ;
+        IsCatchPressed = ik.IsCatchPressed;
+    }
+
+    public override string ToString()
+    {
+        return (MoveToolAngleX, MoveToolAngleY, MoveToolAngleZ, IsCatchPressed).ToString();
     }
 }
